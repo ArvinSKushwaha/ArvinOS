01834ba5e775f20be9876c59a6c85cc61adbacac
Arvin Singh Kushwaha <askushwa@ncsu.edu>
2022-08-02 17:16:33 -0400

    Made simple multiboot2 setup

--- src/main.rs
+++ src/main.rs
@@ -12,18 +12,19 @@ mod output;
 mod multiboot;
 
 global_asm! {r#"
-    .set ALIGN, 1<<0
-    .set MEMINFO, 1<<1
-    .set VIDINFO, 1<<2
-    .set FLAGS, ALIGN | MEMINFO | VIDINFO
-    .set MAGIC, 0x1BADB002
-    .set CHECKSUM, -(MAGIC + FLAGS)
+    .set MAGIC, 0xE85250D6
+    .set ARCHITECTURE, 0
+    .set HEADERLENGTH, 16
+    .set CHECKSUM, 0x17ADAF1A
+    .set FLAGS, 0
 
     .section .multiboot
-    .align 4
+    .align 8
     .long MAGIC
-    .long FLAGS
+    .long ARCHITECTURE
+    .long HEADERLENGTH
     .long CHECKSUM
+    .long FLAGS
 
     .section .bss
     .align 16
@@ -40,7 +41,7 @@ pub fn panic(info: &PanicInfo) -> ! {
 
 /// This method is the portal through which our operating system is executed.
 /// It gets called by [`_start`], which sets up our stack and halt loop.
-pub fn kernel_main(multiboot_data: Multiboot) {
+pub fn kernel_main() {
 }
 
 /// # Safety
@@ -51,15 +52,14 @@ pub unsafe extern "C" fn _start() -> ! {
     // Check if bootloader is multiboot compliant
     let mut eax: u32;
     let mut _ebx: *const Multiboot;
-    core::arch::asm!("mov {x}, eax", "mov {y}, ebx", x = out(reg) eax, y = out(reg) _ebx);
-    assert_eq!(eax, 0x2BADB002);
+    core::arch::asm!("mov {x}, eax", x = out(reg) eax);
+    assert_eq!(eax, 0x36d76289);
 
-    let multiboot_data = *_ebx;
 
     // Set up stack
     core::arch::asm!("mov $stack_top, esp");
 
-    kernel_main(multiboot_data);
+    kernel_main();
 
     intrinsics::halt_loop();
 }
