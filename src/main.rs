b9e200e8f0b0575732051b0dd4a8c787eee05323
Arvin Singh Kushwaha <askushwa@ncsu.edu>
2022-08-02 17:46:05 -0400

    Used rust magic to remove a bunch o' assembly

--- src/main.rs
+++ src/main.rs
@@ -2,30 +2,15 @@
 #![no_main]
 // #![feature(const_mut_refs)]
 #![allow(dead_code)]
+#![feature(used_with_arg)]
 
 use core::{arch::global_asm, panic::PanicInfo};
 
-use crate::multiboot::Multiboot;
-
 mod intrinsics;
+mod multiboot_header;
 mod output;
-mod multiboot;
 
 global_asm! {r#"
-    .set MAGIC, 0xE85250D6
-    .set ARCHITECTURE, 0
-    .set HEADERLENGTH, 16
-    .set CHECKSUM, 0x17ADAF1A
-    .set FLAGS, 0
-
-    .section .multiboot
-    .align 8
-    .long MAGIC
-    .long ARCHITECTURE
-    .long HEADERLENGTH
-    .long CHECKSUM
-    .long FLAGS
-
     .section .bss
     .align 16
     stack_bottom:
@@ -41,8 +26,7 @@ pub fn panic(info: &PanicInfo) -> ! {
 
 /// This method is the portal through which our operating system is executed.
 /// It gets called by [`_start`], which sets up our stack and halt loop.
-pub fn kernel_main() {
-}
+pub fn kernel_main() {}
 
 /// # Safety
 /// This method is meant to be loaded by GRUB, not for use to attempt
@@ -51,10 +35,8 @@ pub fn kernel_main() {
 pub unsafe extern "C" fn _start() -> ! {
     // Check if bootloader is multiboot compliant
     let mut eax: u32;
-    let mut _ebx: *const Multiboot;
     core::arch::asm!("mov {x}, eax", x = out(reg) eax);
     assert_eq!(eax, 0x36d76289);
-
 
     // Set up stack
     core::arch::asm!("mov $stack_top, esp");
